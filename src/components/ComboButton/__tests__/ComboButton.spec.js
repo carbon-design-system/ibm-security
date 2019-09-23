1faa04a92592da2891f1576ccfe7852fefca04bd
Jen Downs <jen.downs@ibm.com>
2019-09-23 10:26:18 -0500

    fix(combo-button): update tests

--- src/components/ComboButton/__tests__/ComboButton.spec.js
+++ src/components/ComboButton/__tests__/ComboButton.spec.js
@@ -3,7 +3,7 @@
  * @copyright IBM Security 2019
  */
 
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import React from 'react';
 import ArrowRight20 from '@carbon/icons-react/lib/arrow--right/20';
 
@@ -11,7 +11,7 @@ import ComboButton, { ComboButtonItem } from '../';
 
 describe('ComboButton', () => {
   it('renders a combo button without an overflow menu', () => {
-    const comboButton = shallow(
+    const comboButton = mount(
       <ComboButton>
         <ComboButtonItem renderIcon={ArrowRight20}>
           Start a task
@@ -32,12 +32,6 @@ describe('ComboButton', () => {
         <ComboButtonItem>Filter list</ComboButtonItem>
       </ComboButton>
     );
-
-    comboButton
-      .find('OverflowMenu')
-      .instance()
-      .setState({ open: true });
-    comboButton.update();
 
     expect(comboButton.find('OverflowMenu').exists()).toEqual(true);
     expect(comboButton).toMatchSnapshot();
