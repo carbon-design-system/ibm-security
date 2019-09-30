12e0413e88cf5641e2ed0d7e116340705cd0077f
Ben Szabo <me@benszabo.co.uk>
2019-09-30 20:24:00 +0100

    feat: add OrderedList
    * added OrderedList component from Carbon Components
    * created corresponding Story

---
+++ src/components/OrderedList/OrderedList.stories.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { components } from '../../../.storybook';
+
+import OrderedList from '../OrderedList';
+import ListItem from '../ListItem';
+
+storiesOf(components('OrderedList'), module)
+  .add(
+    'default',
+    () => (
+      <OrderedList>
+        <ListItem>Ordered List level 1</ListItem>
+        <ListItem>Ordered List level 1</ListItem>
+        <ListItem>Ordered List level 1</ListItem>
+      </OrderedList>
+    ),
+    {
+      info: {
+        text: `Lists consist of related content grouped together and organized vertically. Ordered lists are used to present content in a numbered list.`,
+      },
+    }
+  )
+  .add(
+    'nested',
+    () => (
+      <OrderedList>
+        <ListItem>Ordered List level 1</ListItem>
+        <OrderedList nested>
+          <ListItem>Ordered List level 2</ListItem>
+          <ListItem>Ordered List level 2</ListItem>
+          <OrderedList nested>
+            <ListItem>Ordered List level 3</ListItem>
+            <ListItem>Ordered List level 3</ListItem>
+          </OrderedList>
+        </OrderedList>
+        <ListItem>Ordered List level 1</ListItem>
+        <ListItem>Ordered List level 1</ListItem>
+      </OrderedList>
+    ),
+    {
+      info: {
+        text: `Lists consist of related content grouped together and organized vertically. Ordered lists are used to present content in a numbered list.`,
+      },
+    }
+  );
