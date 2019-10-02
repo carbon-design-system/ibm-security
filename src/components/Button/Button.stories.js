a38069484615c6276aade313009202e014825de2
b3nk3 <b3nk3@icloud.com>
2019-10-02 11:35:22 +0100

    style: change to sentence case for consistency
    Co-Authored-By: Simon Finney <simonjfinney@gmail.com>

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -28,7 +28,7 @@ const kinds = {
   'Secondary button (secondary)': 'secondary',
   'Danger button (danger)': 'danger',
   'Ghost button (ghost)': 'ghost',
-  'Ghost Danger button (ghost-danger)': 'ghost-danger',
+'Ghost danger button (ghost-danger)': 'ghost-danger',
 };
 
 const sizes = {
