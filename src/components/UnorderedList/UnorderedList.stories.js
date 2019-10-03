9c25a31e5b961d700dc91e409eea9de8140c168d
b3nk3 <b3nk3@icloud.com>
2019-10-03 07:57:59 +0100

    fix: enhance comment
    Co-Authored-By: Jen Downs <hello@jendowns.com>

--- src/components/UnorderedList/UnorderedList.stories.js
+++ src/components/UnorderedList/UnorderedList.stories.js
@@ -3,6 +3,9 @@
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
+/**
+ * @file Unordered list stories.
+ * @copyright IBM Security 2019
  */
 
 import React from 'react';
