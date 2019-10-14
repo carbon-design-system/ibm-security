d6ab5b58325cf4344106238c4a704d0e7b515dfb
Jen Downs <jen.downs@ibm.com>
2019-10-14 15:37:05 -0500

    feat: export tooltip definition, replace prev usage with string formatter and aria-label + title

--- src/components/Header/HeaderNotification/HeaderNotification.js
+++ src/components/Header/HeaderNotification/HeaderNotification.js
@@ -4,21 +4,12 @@
  */
 
 import Close20 from '@carbon/icons-react/lib/close/20';
-import { TooltipDefinition } from 'carbon-components-react';
-
 import React from 'react';
-import Truncate from 'react-truncate';
 
 import Icon from '../../Icon';
+import StringFormatter from '../../StringFormatter';
 
 import { namespace, propTypes } from './constants';
-
-/**
- * Header notification component.
- * @param {Object.<string, *>} props Header notification props.
- * @returns {HeaderNotification} Header notification instance.
- */
-const tooltipLeftOffset = 16;
 
 const HeaderNotification = ({
   clearButtonLabel,
@@ -28,90 +19,35 @@ const HeaderNotification = ({
   product,
   viaLabel,
   href,
-}) => {
-  function setTooltipPosition(e) {
-    const target = e.target
-      .closest(`.${namespace}`)
-      .querySelector(`.${namespace}__tooltip`);
-    if (target) {
-      const tooltip = target.querySelector(`.${namespace}__tooltip--body`);
-      if (tooltip) {
-        const { top, left } = target.getBoundingClientRect();
-        tooltip.style.top = `${top}px`;
-        tooltip.style.left = `${left + tooltipLeftOffset}px`;
-        const button = tooltip.querySelector('button');
-        if (button) button.setAttribute('tabIndex', '-1');
-      }
-    }
-  }
-
-  function showTooltip(e) {
-    const tooltip = e.target.querySelector(`.${namespace}__tooltip--body`);
-    if (tooltip) {
-      const { top, left } = e.target.getBoundingClientRect();
-      tooltip.style.top = `${top}px`;
-      tooltip.style.left = `${left + tooltipLeftOffset}px`;
-      const button = tooltip.querySelector('button');
-      if (button) button.setAttribute('tabIndex', '-1');
-    }
-  }
-
-  return (
-    <div className={namespace} role="alert">
-      <a
-        role="button"
-        tabIndex="0"
-        {...(href ? { href } : {})}
-        className={`${namespace}__content`}
-        onBlur={setTooltipPosition}
+}) => (
+  <div className={namespace} role="alert">
+    <a {...(href ? { href } : {})} className={`${namespace}__content`}>
+      <span className={`${namespace}__details`}>
+        <time className={`${namespace}__time`} dateTime={dateTime}>
+          <isc-rel-time datetime={dateTime} />
+        </time>
+        {` ${viaLabel} ${product}`}
+      </span>
+      <StringFormatter
+        className={`${namespace}__description`}
+        lines={2}
+        truncate
+        title={description}
+        aria-label={description}
+        value={description}
+      />
+    </a>
+    <div className={`${namespace}__wrapper`}>
+      <button
+        className={`${namespace}__button`}
+        aria-label={clearButtonLabel}
+        onClick={onClearButtonClick}
       >
-        <span className={`${namespace}__details`}>
-          <time className={`${namespace}__time`} dateTime={dateTime}>
-            <isc-rel-time datetime={dateTime} />
-          </time>
-          {` ${viaLabel} ${product}`}
-        </span>
-        <Truncate
-          className={`${namespace}__description`}
-          lines={2}
-          width={170}
-          ellipsis={
-            <div
-              className={`${namespace}__tooltip`}
-              onMouseOver={showTooltip}
-              onFocus={showTooltip}
-              role="button"
-              tabIndex="0"
-            >
-              {'...'}
-              <TooltipDefinition
-                className={`${namespace}__tooltip--body`}
-                tooltipText={description}
-                align="end"
-                direction="top"
-              >
-                {''}
-              </TooltipDefinition>
-            </div>
-          }
-          trimWhitespace
-        >
-          {description}
-        </Truncate>
-      </a>
-      <div className={`${namespace}__wrapper`}>
-        <button
-          className={`${namespace}__button`}
-          aria-label={clearButtonLabel}
-          onClick={onClearButtonClick}
-          onBlur={setTooltipPosition}
-        >
-          <Icon renderIcon={Close20} />
-        </button>
-      </div>
+        <Icon renderIcon={Close20} />
+      </button>
     </div>
-  );
-};
+  </div>
+);
 
 HeaderNotification.propTypes = propTypes;
 
