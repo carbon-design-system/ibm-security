0c89f0bb928816f6a54c17b9dad3b19798abb9bd
Simon Finney <SIMONFIN@ie.ibm.com>
2019-10-21 14:34:41 +0100

    fix(search-bar): modularise styles

--- .storybook/components/Container/index.js
+++ .storybook/components/Container/index.js
@@ -7,7 +7,7 @@
 // import '../../index.scss';
 
 // TODO: `modular-styles` - Remove import.
-// import '../../../src/components/Pill/_index.scss';
+// import '../../../src/components/SearchBar/_index.scss';
 
 const Container = ({ children }) => children;
 
