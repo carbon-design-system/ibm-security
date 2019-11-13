a7793896cd6dca72bbdea51a9a183f60ce6d3162
Simon Finney <SIMONFIN@ie.ibm.com>
2019-11-13 18:21:07 +0000

    feat(modular-styles): update `DatePicker`, `Tile`, and `Toggle`

--- .storybook/components/Container/index.js
+++ .storybook/components/Container/index.js
@@ -7,7 +7,7 @@
 // import '../../index.scss';
 
 // TODO: `modular-styles` - Remove import.
-// import '../../../src/components/Wizard/_index.scss';
+import '../../../src/components/DatePicker/_index.scss';
 
 const Container = ({ children }) => children;
 
