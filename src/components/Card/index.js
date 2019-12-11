24533ee2800ceb81f0174d88163c0bc7e391e0da
Simon Finney <SIMONFIN@ie.ibm.com>
2019-12-11 13:01:38 +0000

    refactor(card): update export to align with other components
    BREAKING CHANGE: `Card` export has been updated to align with other components.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,9 +3,5 @@
  * @copyright IBM Security 2019
  */
 
-// TODO: `2.x` - Fix export.
-import Card from './Card';
-import CardSkeleton from './CardSkeleton/CardSkeleton';
-
-export default Card;
-export { Card, CardSkeleton };
+export default from './Card';
+export CardSkeleton from './CardSkeleton/CardSkeleton';
