d0a295e904374fc1cf34bb89224e61fb1b6e4de9
Jen Downs <hello@jendowns.com>
2020-01-23 11:09:10 -0600

    fix(panelv2): update test structure for onclose

--- src/components/PanelV2/__tests__/PanelV2.spec.js
+++ src/components/PanelV2/__tests__/PanelV2.spec.js
@@ -9,62 +9,68 @@ import React from 'react';
 
 import { Button, PanelV2, PanelContent } from '../../..';
 
-const PanelExample = () => (
-  <PanelV2
-    title="Title"
-    subtitle="Subtitle"
-    className="custom-class"
-    closeButton={{
-      label: 'Close',
-      onClick: () => {},
-    }}
-    renderFooter={() => <Button onClick={() => {}}>Add</Button>}
-  >
-    <PanelContent>Content</PanelContent>
-  </PanelV2>
-);
-
 describe('PanelV2', () => {
   afterEach(cleanup);
 
   test('should render as expected', async () => {
     const main = document.createElement('main');
-    render(<PanelExample open />, {
-      container: document.body.appendChild(main),
-    });
+    render(
+      <PanelV2
+        title="Title"
+        subtitle="Subtitle"
+        closeButton={{
+          label: 'Close',
+          onClick: () => {},
+        }}
+        renderFooter={() => <Button onClick={() => {}}>Add</Button>}
+      >
+        <PanelContent>Content</PanelContent>
+      </PanelV2>,
+      {
+        container: document.body.appendChild(main),
+      }
+    );
     await expect(document.body).toMatchSnapshot();
   });
 
   test('should have no Axe or DAP violations', async () => {
     const main = document.createElement('main');
-    render(<PanelExample open />, {
-      container: document.body.appendChild(main),
-    });
+    render(
+      <PanelV2
+        title="test title"
+        subtitle="test subtitle"
+        closeButton={{
+          label: 'test close',
+          onClick: () => {},
+        }}
+        renderFooter={() => <Button onClick={() => {}}>Add</Button>}
+      >
+        <PanelContent>Content</PanelContent>
+      </PanelV2>,
+      {
+        container: document.body.appendChild(main),
+      }
+    );
     await expect(document.body).toHaveNoAxeViolations();
     await expect(document.body).toHaveNoDAPViolations('PanelV2');
   });
 
   test('should invoke `onClose`', () => {
-    const main = document.createElement('main');
     const onCloseMock = jest.fn();
-    render(
-      <PanelExample
+    const { getByLabelText, getByText } = render(
+      <PanelV2
         open
+        title="test title"
         closeButton={{
-          label: 'Close',
+          label: 'test close',
           onClick: onCloseMock,
         }}
-      />,
-      { container: document.body.appendChild(main) }
+      />
     );
 
-    userEvent.click(
-      document.body.querySelector('.security--panelv2__button--close')
-    );
+    userEvent.click(getByLabelText(/test close/i));
 
     expect(onCloseMock).toHaveBeenCalled();
-    // await expect(
-    //   document.body.querySelector(".security--panelv2")
-    // ).not.toBeInTheDocument();
+    expect(getByText(/test title/i)).toBeInTheDocument();
   });
 });
