956a9f7ca771c5efcd44433479700662fc8b0339
Jen Downs <hello@jendowns.com>
2020-01-24 11:56:31 -0600

    fix(unstable-pagination): add tests for next/previous buttons

--- src/components/UNSTABLE__Pagination/__tests__/UNSTABLE_Pagination.spec.js
+++ src/components/UNSTABLE__Pagination/__tests__/UNSTABLE_Pagination.spec.js
@@ -45,7 +45,7 @@ describe('UNSTABLE_Pagination', () => {
       </UNSTABLE__Pagination>
     );
 
-    // Find the "page sizer" that defines "items per page":
+    // Find the "page sizer" select that defines "items per page":
     const pageSizer = document.getElementById(
       'security--unstable-pagination__page-sizer__input-1'
     );
@@ -96,13 +96,135 @@ describe('UNSTABLE_Pagination', () => {
     // showing 5 items per page out of 10 total items.
     expect(getByText(/1–5 of 10 items/i)).toBeInTheDocument();
 
+    // Expect the default displayed current page to be page 1:
+    expect(getByLabelText(/test page selector/i).value).toEqual('1');
+
     // Go to page 2:
     userEvent.selectOptions(getByLabelText(/test page selector/i), ['2']);
 
     // Expect the page selector to show that we are on page 2:
     expect(getByLabelText(/test page selector/i).value).toEqual('2');
 
-    // Expect to see updated text show current items:
+    // Expect to see updated text showing current items on current page:
     expect(getByText(/6–10 of 10 items/i)).toBeInTheDocument();
+  });
+
+  test('should go to the next page when "next" button is clicked', () => {
+    const { getByText, getByLabelText } = render(
+      <UNSTABLE__Pagination
+        forwardText="test next button"
+        totalItems={10}
+        pageSize={5}
+        pageSizes={[5]}
+      >
+        {({ currentPage, onSetPage, totalPages }) => (
+          <PageSelector
+            currentPage={currentPage}
+            labelText="test page selector"
+            onChange={event => onSetPage(event.target.value)}
+            totalPages={totalPages}
+          />
+        )}
+      </UNSTABLE__Pagination>
+    );
+
+    // Expect to see default text for page 1:
+    expect(getByText(/1–5 of 10 items/i)).toBeInTheDocument();
+
+    // Expect the default displayed current page to be page 1:
+    expect(getByLabelText(/test page selector/i).value).toEqual('1');
+
+    // Click on the "next" button to go to page 2:
+    userEvent.click(getByText(/test next button/i));
+
+    // Expect the page selector to show that we are on page 2:
+    expect(getByLabelText(/test page selector/i).value).toEqual('2');
+
+    // Expect to see updated text showing current items on page 1:
+    expect(getByText(/6–10 of 10 items/i)).toBeInTheDocument();
+  });
+
+  test('should go to the previous page when "previous" button is clicked', () => {
+    const { getByText, getByLabelText } = render(
+      <UNSTABLE__Pagination
+        initialPage={2}
+        backwardText="test previous button"
+        totalItems={10}
+        pageSize={5}
+        pageSizes={[5]}
+      >
+        {({ currentPage, onSetPage, totalPages }) => (
+          <PageSelector
+            currentPage={currentPage}
+            labelText="test page selector"
+            onChange={event => onSetPage(event.target.value)}
+            totalPages={totalPages}
+          />
+        )}
+      </UNSTABLE__Pagination>
+    );
+
+    // Expect to see text for page 2,
+    // because we set the `initialPage={2}`:
+    expect(getByText(/6–10 of 10 items/i)).toBeInTheDocument();
+
+    // Expect the default displayed current page to be page 2,
+    // because we set the `initialPage={2}`:
+    expect(getByLabelText(/test page selector/i).value).toEqual('2');
+
+    // Click on the "previous" button to go back to page 1:
+    userEvent.click(getByText(/test previous button/i));
+
+    // Expect the page selector to show that we are on page 1:
+    expect(getByLabelText(/test page selector/i).value).toEqual('1');
+
+    // Expect to see updated text showing current items on page 1:
+    expect(getByText(/1–5 of 10 items/i)).toBeInTheDocument();
+  });
+
+  test('should disable backwards navigation on the first page', () => {
+    const { getByText } = render(
+      <UNSTABLE__Pagination
+        backwardText="test previous button"
+        forwardText="test next button"
+        totalItems={10}
+        pageSize={5}
+        pageSizes={[5]}
+      />
+    );
+
+    // Expect "previous" button to be disabled because we are on the first page:
+    expect(
+      getByText(/test previous button/i).closest('button')
+    ).toHaveAttribute('disabled');
+
+    // Expect "next" button to NOT be disabled:
+    expect(
+      getByText(/test next button/i).closest('button')
+    ).not.toHaveAttribute('disabled');
+  });
+
+  test('should disable forward navigation on the last page', () => {
+    const { getByText } = render(
+      <UNSTABLE__Pagination
+        // Note that initial page is set to 2, the last page:
+        initialPage={2}
+        backwardText="test previous button"
+        forwardText="test next button"
+        totalItems={10}
+        pageSize={5}
+        pageSizes={[5]}
+      />
+    );
+
+    // Expect "previous" button to NOT disabled:
+    expect(
+      getByText(/test previous button/i).closest('button')
+    ).not.toHaveAttribute('disabled');
+
+    // Expect "next" button to be disabled because we are on the last page:
+    expect(getByText(/test next button/i).closest('button')).toHaveAttribute(
+      'disabled'
+    );
   });
 });
