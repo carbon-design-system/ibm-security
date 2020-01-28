7ec0b759cc4fc11c37c7724c4a63827a77898023
Jen Downs <hello@jendowns.com>
2020-01-28 09:45:08 -0600

    fix(panel-v2): update tests to include changes to panel content wrapper and scrolling content settings

--- src/components/PanelV2/__tests__/PanelV2.spec.js
+++ src/components/PanelV2/__tests__/PanelV2.spec.js
@@ -30,7 +30,9 @@ describe('PanelV2', () => {
       }
     );
     await expect(document.body).toHaveNoAxeViolations();
-    await expect(document.body).toHaveNoDAPViolations('PanelV2');
+    await expect(document.body).toHaveNoDAPViolations(
+      'PanelV2 with renderFooter'
+    );
   });
 
   test('should have no Axe or DAP violations with `title` as a `node`', async () => {
@@ -51,7 +53,9 @@ describe('PanelV2', () => {
       }
     );
     await expect(document.body).toHaveNoAxeViolations();
-    await expect(document.body).toHaveNoDAPViolations('PanelV2');
+    await expect(document.body).toHaveNoDAPViolations(
+      'PanelV2 with title as node'
+    );
   });
 
   test('should have no Axe or DAP violations with deprecated `primaryButton` & `secondaryButton`', async () => {
@@ -80,7 +84,9 @@ describe('PanelV2', () => {
       }
     );
     await expect(document.body).toHaveNoAxeViolations();
-    await expect(document.body).toHaveNoDAPViolations('PanelV2');
+    await expect(document.body).toHaveNoDAPViolations(
+      'PanelV2 with deprecated props'
+    );
   });
 
   test('should invoke close mock when close button is clicked', () => {
@@ -134,8 +140,51 @@ describe('PanelV2', () => {
 
     userEvent.tab();
 
-    // The `PanelContent` wrapper:
-    expect(getByText(/test content text/i)).toHaveFocus();
+    // The button inside the `PanelContent` wrapper:
+    expect(getByText(/test content button/i)).toHaveFocus();
+
+    userEvent.tab();
+
+    // The footer button:
+    expect(getByText(/test footer button/i)).toHaveFocus();
+
+    userEvent.tab();
+
+    // Loop complete.
+    // The close button:
+    expect(getByLabelText(/test close/i)).toHaveFocus();
+  });
+
+  test('should cycle panel elements in tab order when there is scrolling content', () => {
+    const { getByLabelText, getByText } = render(
+      <PanelV2
+        closeButton={{
+          label: 'test close',
+        }}
+        renderFooter={() => <Button>test footer button</Button>}
+        // Note that `hasScrollingContent={true}` so the `PanelContent` wrapper is tabbable:
+        hasScrollingContent
+        // `aria-label` provided because there is scrolling content:
+        aria-label="test aria-label"
+      >
+        <PanelContent>
+          test content text
+          <Button>test content button</Button>
+        </PanelContent>
+      </PanelV2>
+    );
+
+    userEvent.tab();
+
+    // The close button:
+    expect(getByLabelText(/test close/i)).toHaveFocus();
+
+    userEvent.tab();
+
+    // The tabbable `PanelContent` wrapper.
+    // Need to use query selector that targets the accessible role
+    // of the scrolling content due to divs wrapping inner text:
+    expect(document.querySelector('section[role="region"]')).toHaveFocus();
 
     userEvent.tab();
 
