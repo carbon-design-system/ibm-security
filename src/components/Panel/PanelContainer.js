b8887d1730e85236d6ad915e79e249aa28b6d5fe
Jen Downs <hello@jendowns.com>
2020-01-29 05:19:52 -0600

    fix(panel): only apply aria-labelledby if title or subtitle provided (#333)
    * fix(panel): only apply aria-labelledBy if valid title or subtitle
    
    * fix(panel): update aria-labelledby
    
    Co-authored-by: Simon Finney <simonjfinney@gmail.com>

--- src/components/Panel/PanelContainer.js
+++ src/components/Panel/PanelContainer.js
@@ -159,14 +159,18 @@ export default class PanelContainer extends Component {
 
     const ariaLabel = this.props['aria-label'] || title || subtitle;
 
-    const getAriaLabelledBy = title ? this.panelTitleId : this.panelSubtitleId;
+    const getAriaLabelledBy =
+      title || subtitle
+        ? {
+            'aria-labelledby': title ? this.panelTitleId : this.panelSubtitleId,
+          }
+        : {};
 
     const hasScrollingContentProps = hasScrollingContent
       ? {
           tabIndex: 0,
           role: 'region',
           'aria-label': ariaLabel,
-          'aria-labelledby': getAriaLabelledBy,
         }
       : {};
 
@@ -209,7 +213,7 @@ export default class PanelContainer extends Component {
             marginBottom: `${this.state.bodyMargin.bottom}px`,
           }}
           {...hasScrollingContentProps}
-          aria-labelledby={getAriaLabelledBy}
+          {...getAriaLabelledBy}
         >
           {children}
         </section>
