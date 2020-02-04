c9a35737ed0202ca85f19a02c947b815a918211f
Jen Downs <hello@jendowns.com>
2020-02-04 14:32:01 -0600

    fix(data-decorator): add a11y tests and remove snapshots

--- src/components/DataDecorator/__tests__/DataDecorator.spec.js
+++ src/components/DataDecorator/__tests__/DataDecorator.spec.js
@@ -3,25 +3,102 @@
  * @copyright IBM Security 2019
  */
 
-import { mount } from 'enzyme';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 
-import { DataDecorator } from '../../..';
-
-import props from '../_mocks_';
+import { Button, DataDecorator } from '../../..';
 
 describe('DataDecorator', () => {
-  let dataDecorator;
+  test('should have no Axe or DAP violations', async () => {
+    const main = document.createElement('main');
+    render(<DataDecorator type="IP" value="10.0.0.0" score={0} inert />, {
+      // DAP requires a landmark '<main>' in the DOM:
+      container: document.body.appendChild(main),
+    });
 
-  beforeEach(() => {
-    dataDecorator = mount(<DataDecorator {...props} />);
+    await expect(document.body).toHaveNoAxeViolations();
+    await expect(document.body).toHaveNoDAPViolations('DataDecorator');
   });
 
-  it('renders correctly', () => {
-    expect(dataDecorator).toMatchSnapshot();
+  test('should have no Axe or DAP violations when active', async () => {
+    const main = document.createElement('main');
+    const { getByText } = render(
+      <DataDecorator
+        type="IP"
+        value="10.0.0.0"
+        score={0}
+        href="#"
+        title="test title"
+      />,
+      {
+        // DAP requires a landmark '<main>' in the DOM:
+        container: document.body.appendChild(main),
+      }
+    );
+
+    // Click on the data decorator to open the connected panel:
+    userEvent.click(getByText(/10.0.0.0/i).closest('button'));
+
+    await expect(document.body).toHaveNoAxeViolations();
+    await expect(document.body).toHaveNoDAPViolations('DataDecorator');
   });
 
-  it("renders the HTML of the node's subtree", () => {
-    expect(dataDecorator.render()).toMatchSnapshot();
+  test('should cycle between data decorator and open panel in tab order', () => {
+    const { getByLabelText, getByText } = render(
+      <DataDecorator
+        type="IP"
+        value="10.0.0.0"
+        score={0}
+        href="#"
+        title="test title"
+        closeButton={{
+          label: 'test close',
+        }}
+      >
+        <Button>test content button</Button>
+      </DataDecorator>
+    );
+
+    userEvent.tab();
+
+    // The data decorator:
+    expect(getByText(/10.0.0.0/i).closest('button')).toHaveFocus();
+
+    userEvent.tab();
+
+    // Expect the data decorator to still have focus
+    // becuase it is currently the only interactive element:
+    expect(getByText(/10.0.0.0/i).closest('button')).toHaveFocus();
+
+    // Simulate a click to open the connected panel.
+    // Note: need to use click event because "Enter"/"Space" keys aren't testing as expected.
+    userEvent.click(getByText(/10.0.0.0/i).closest('button'));
+
+    // Tab into the open panel.
+    userEvent.tab();
+
+    // The panel's close button:
+    expect(getByLabelText(/test close/i)).toHaveFocus();
+
+    userEvent.tab();
+
+    // The example button inside the panel:
+    expect(getByText(/test content button/i)).toHaveFocus();
+
+    userEvent.tab();
+
+    // Panel loop complete.
+    // Focus cannot leave panel until it is closed.
+    expect(getByLabelText(/test close/i)).toHaveFocus();
+
+    // Simulate a click to close the panel.
+    // Note: need to use click event because "Enter"/"Space" keys aren't testing as expected.
+    userEvent.click(getByLabelText(/test close/i));
+
+    userEvent.tab();
+
+    // Expect the data decorator to have focus again:
+    expect(getByText(/10.0.0.0/i).closest('button')).toHaveFocus();
   });
 });
