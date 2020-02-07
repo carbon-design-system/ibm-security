31fac969c59e77795ee65947e2738b2913191ce6
Jen Downs <hello@jendowns.com>
2020-02-07 11:44:11 -0600

    fix(nonentitled-section): add a11y test and remove snapshots

--- src/components/NonEntitledSection/__tests__/NonEntitledSection.spec.js
+++ src/components/NonEntitledSection/__tests__/NonEntitledSection.spec.js
@@ -3,28 +3,41 @@
  * @copyright IBM Security 2019
  */
 
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
 import React from 'react';
-import NonEntitledSection from '../NonEntitledSection';
 
-import { noSubscriptionExample } from '../_mocks_';
+import { icon } from '../../_mocks_';
+import { NonEntitledSection } from '../../..';
 
-describe('NonEntitledSection', () => {
-  let nonEntitledSection;
-
-  beforeEach(() => {
-    nonEntitledSection = shallow(
-      <NonEntitledSection {...noSubscriptionExample} />
+describe('Decorator', () => {
+  test('should have no Axe or DAP violations', async () => {
+    const main = document.createElement('main');
+    render(
+      <NonEntitledSection
+        title="test title"
+        subTitle="test subtitle"
+        description="test description"
+        links={[
+          {
+            id: 'test-link-1',
+            text: 'test link 1',
+            href: '#',
+          },
+          {
+            id: 'test-link-2',
+            text: 'test link 2',
+            href: '#',
+            icon,
+          },
+        ]}
+      />,
+      {
+        // DAP requires a landmark '<main>' in the DOM:
+        container: document.body.appendChild(main),
+      }
     );
-  });
 
-  describe('render', () => {
-    it('renders correctly', () => {
-      expect(nonEntitledSection).toMatchSnapshot();
-    });
-
-    it("renders the HTML of the node's subtree", () => {
-      expect(nonEntitledSection.render()).toMatchSnapshot();
-    });
+    await expect(document.body).toHaveNoAxeViolations();
+    await expect(document.body).toHaveNoDAPViolations('NonEntitledSection');
   });
 });
