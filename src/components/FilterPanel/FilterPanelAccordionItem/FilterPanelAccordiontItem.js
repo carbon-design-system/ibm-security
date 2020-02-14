7ecc8c2db050051928632e79748c4f5df0b38ae7
Jen Downs <hello@jendowns.com>
2020-02-14 14:39:12 -0600

    fix(filter-panel-accordion-item): reorder elements so li are only children of ul elems (#377)

--- src/components/FilterPanel/FilterPanelAccordionItem/FilterPanelAccordiontItem.js
+++ src/components/FilterPanel/FilterPanelAccordionItem/FilterPanelAccordiontItem.js
@@ -85,37 +85,39 @@ const FilterPanelAccordionItem = ({
       open={open}
       {...other}
     >
-      <ul className={`${namespace}__list`}>
-        <ScrollGradient
-          scrollElementClassName={`${namespace}__scroller`}
-          getScrollElementRef={setListContainer}
-          color={scrollGradientColor}
+      <ScrollGradient
+        scrollElementClassName={`${namespace}__scroller`}
+        getScrollElementRef={setListContainer}
+        color={scrollGradientColor}
+      >
+        <div
+          role="presentation"
+          ref={visibleChildren}
+          className={`${namespace}__list-items ${namespace}__list-items--visible`}
         >
-          <div
-            role="presentation"
-            ref={visibleChildren}
-            className={`${namespace}__list-items ${namespace}__list-items--visible`}
-          >
+          <ul className={`${namespace}__list`}>
             {childrenArray.slice(0, displayCount).map(child => (
               <li className={`${namespace}__list-item`} key={child.key}>
                 {child}
               </li>
             ))}
-          </div>
-          {shouldTruncate && isExpanded && (
-            <div
-              role="presentation"
-              className={`${namespace}__list-items ${namespace}__list-items--hidden`}
-            >
+          </ul>
+        </div>
+        {shouldTruncate && isExpanded && (
+          <div
+            role="presentation"
+            className={`${namespace}__list-items ${namespace}__list-items--hidden`}
+          >
+            <ul className={`${namespace}__list`}>
               {childrenArray.slice(displayCount).map(child => (
                 <li className={`${namespace}__filter`} key={child.key}>
                   {child}
                 </li>
               ))}
-            </div>
-          )}
-        </ScrollGradient>
-      </ul>
+            </ul>
+          </div>
+        )}
+      </ScrollGradient>
       {shouldTruncate && (
         <Button
           className={`${namespace}__button--toggle`}
