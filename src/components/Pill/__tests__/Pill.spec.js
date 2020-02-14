75d3140222c8c7c0a95ad95fe4680ff766179e15
Jen Downs <hello@jendowns.com>
2020-02-14 09:27:33 -0600

    fix(pill): add inline, type, and noIcon tests

--- src/components/Pill/__tests__/Pill.spec.js
+++ src/components/Pill/__tests__/Pill.spec.js
@@ -8,6 +8,7 @@ import React from 'react';
 import userEvent from '@testing-library/user-event';
 
 import { Pill } from '../../..';
+import { namespace } from '../Pill';
 
 describe('Pill', () => {
   test('should have no Axe or DAP violations', async () => {
@@ -24,8 +25,24 @@ describe('Pill', () => {
     const { getByText } = render(
       <Pill value="127.0.0.1" type="IP" className="custom-class" />
     );
-
     expect(getByText(/127.0.0.1/i).parentNode).toHaveClass('custom-class');
+  });
+
+  test('should apply type', () => {
+    const { getByText } = render(
+      <Pill value="127.0.0.1" type="IP" className="custom-class" />
+    );
+    expect(getByText(/IP/i)).toBeVisible();
+  });
+
+  test('should be inline when `inline` is set to `true`', () => {
+    const { container } = render(<Pill value="127.0.0.1" type="IP" inline />);
+    expect(container.firstElementChild).toHaveClass(`${namespace}--inline`);
+  });
+
+  test('should not have an icon', () => {
+    const { container } = render(<Pill value="127.0.0.1" type="IP" />);
+    expect(container.querySelector('svg')).toBe(null);
   });
 
   test('should be inert and non-interactive', () => {
@@ -35,15 +52,11 @@ describe('Pill', () => {
         <button>test button</button>
       </div>
     );
-
     userEvent.tab();
-
     // Expect only the extra test button to be interactive:
     expect(getByText(/127.0.0.1/i).parentNode).not.toHaveFocus();
     expect(getByText(/test button/i)).toHaveFocus();
-
     userEvent.tab();
-
     // Still expect only the extra test button to be interactive:
     expect(getByText(/127.0.0.1/i).parentNode).not.toHaveFocus();
     expect(getByText(/test button/i)).toHaveFocus();
