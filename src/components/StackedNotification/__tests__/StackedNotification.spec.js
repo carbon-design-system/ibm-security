b37dd5af813f2f9ef98b2516d348f958b039fd61
Jen Downs <hello@jendowns.com>
2020-02-17 15:24:25 -0600

    fix(stacked-notification): update tests and remove old snapshots

--- src/components/StackedNotification/__tests__/StackedNotification.spec.js
+++ src/components/StackedNotification/__tests__/StackedNotification.spec.js
@@ -3,33 +3,66 @@
  * @copyright IBM Security 2019
  */
 
-import { shallow } from 'enzyme';
 import React from 'react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-import { iconDescription, title, subtitle } from '../_mocks_';
-
-import StackedNotification from '../';
+import { StackedNotification } from '../../..';
 
 describe('StackedNotification', () => {
-  let stackedNotification;
-
-  beforeEach(() => {
-    stackedNotification = shallow(
+  test('should have no Axe or DAP violations', async () => {
+    const main = document.createElement('main');
+    render(
       <StackedNotification
-        iconDescription={iconDescription}
-        title={title}
-        subtitle={subtitle}
-      />
+        title="test title"
+        subtitle="test subtitle"
+        caption="test caption"
+        iconDescription="test close button icon"
+      />,
+      {
+        // DAP requires a landmark '<main>' in the DOM:
+        container: document.body.appendChild(main),
+      }
     );
+    await expect(document.body).toHaveNoAxeViolations();
+    await expect(document.body).toHaveNoDAPViolations('StackedNotification');
   });
 
-  describe('render', () => {
-    it('renders correctly', () => {
-      expect(stackedNotification).toMatchSnapshot();
-    });
+  test('should add a custom class', () => {
+    const { container } = render(
+      <StackedNotification
+        title="test title"
+        className="custom-class"
+        subtitle="test subtitle"
+        iconDescription="test icon desc"
+      />
+    );
+    expect(container.firstElementChild).toHaveClass('custom-class');
+  });
 
-    it("renders the HTML of the node's subtree", () => {
-      expect(stackedNotification.render()).toMatchSnapshot();
-    });
+  test('should invoke close mock when close button is clicked', () => {
+    const onCloseMock = jest.fn();
+    const { getByLabelText } = render(
+      <StackedNotification
+        title="test title"
+        subtitle="test subtitle"
+        iconDescription="test close button icon"
+        onCloseButtonClick={onCloseMock}
+      />
+    );
+    userEvent.click(getByLabelText(/test close button icon/i));
+    expect(onCloseMock).toHaveBeenCalledTimes(1);
+  });
+
+  test('should pass through extra props via spread attribute', () => {
+    const { queryByTestId } = render(
+      <StackedNotification
+        title="test title"
+        subtitle="test subtitle"
+        iconDescription="test close button icon"
+        data-testid="test-id"
+      />
+    );
+    expect(queryByTestId('test-id')).toBeInTheDocument();
   });
 });
