44c55ce853154ad6a72b8508ad707d2b115a3dcf
Jen Downs <hello@jendowns.com>
2020-02-19 13:46:35 -0600

    fix(profile-image): add a11y tests, update test structure, add spread attribute

--- src/components/ProfileImage/__tests__/ProfileImage.spec.js
+++ src/components/ProfileImage/__tests__/ProfileImage.spec.js
@@ -3,28 +3,106 @@
  * @copyright IBM Security 2019
  */
 
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
 import React from 'react';
 
-import { className, large, profile } from '../_mocks_';
+import { ProfileImage } from '../../..';
 
-import ProfileImage from '..';
+import { namespace } from '../ProfileImage';
 
 describe('ProfileImage', () => {
-  let profileImage;
-
-  beforeEach(() => {
-    profileImage = shallow(
-      <ProfileImage className={className} profile={profile} />
+  test('should have no Axe or DAP violations when image is NOT provided', async () => {
+    const main = document.createElement('main');
+    render(
+      <ProfileImage
+        profile={{
+          image_url: null,
+          name: {
+            first_name: 'Test',
+            surname: 'User',
+          },
+        }}
+      />,
+      {
+        // DAP requires a landmark '<main>' in the DOM:
+        container: document.body.appendChild(main),
+      }
+    );
+    await expect(document.body).toHaveNoAxeViolations();
+    await expect(document.body).toHaveNoDAPViolations(
+      'ProfileImage without image'
     );
   });
 
-  it('renders', () => {
-    expect(profileImage).toMatchSnapshot();
+  test('should have no Axe or DAP violations when image is provided', async () => {
+    const main = document.createElement('main');
+    render(
+      <ProfileImage
+        profile={{
+          image_url: 'example.svg',
+          name: {
+            first_name: 'Test',
+            surname: 'User',
+          },
+        }}
+      />,
+      {
+        // DAP requires a landmark '<main>' in the DOM:
+        container: document.body.appendChild(main),
+      }
+    );
+    await expect(document.body).toHaveNoAxeViolations();
+    await expect(document.body).toHaveNoDAPViolations(
+      'ProfileImage with image'
+    );
   });
 
-  it('renders the large variation', () => {
-    profileImage.setProps({ large });
-    expect(profileImage.render()).toMatchSnapshot();
+  test('should add a custom class', () => {
+    const { container } = render(
+      <ProfileImage
+        data-testid="test-id"
+        className="custom-class"
+        profile={{
+          image_url: null,
+          name: {
+            first_name: 'Test',
+            surname: 'User',
+          },
+        }}
+      />
+    );
+    expect(container.firstElementChild).toHaveClass('custom-class');
+  });
+
+  test('should add correct class when `large` is `true`', () => {
+    const { container } = render(
+      <ProfileImage
+        profile={{
+          image_url: null,
+          name: {
+            first_name: 'Test',
+            surname: 'User',
+          },
+        }}
+        large
+      />
+    );
+    expect(container.firstElementChild).toHaveClass(`${namespace}--large`);
+  });
+
+  test('should pass through extra props via spread attribute', () => {
+    const { queryByTestId } = render(
+      <ProfileImage
+        data-testid="test-id"
+        profile={{
+          image_url: null,
+          name: {
+            first_name: 'Test',
+            surname: 'User',
+          },
+        }}
+      />
+    );
+    expect(queryByTestId('test-id')).toBeInTheDocument();
   });
 });
