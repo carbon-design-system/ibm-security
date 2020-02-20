fd1fc4082374b2edc4345f07d6d4bbf53d9ad757
Jen Downs <hello@jendowns.com>
2020-02-20 10:36:53 -0600

    fix(step-indicator): add test for step custom class

--- src/components/StepIndicator/__tests__/StepIndicator.spec.js
+++ src/components/StepIndicator/__tests__/StepIndicator.spec.js
@@ -11,7 +11,7 @@ import { Step, StepIndicator } from '../../..';
 describe('StepIndicator', () => {
   test('should have no Axe or DAP violations', async () => {
     const main = document.createElement('main');
-    const { debug } = render(
+    render(
       <StepIndicator currentIndex={1}>
         <Step label="test label 1" description="test description 1" />
         <Step label="test label 2" description="test description 2" />
@@ -21,7 +21,6 @@ describe('StepIndicator', () => {
         container: document.body.appendChild(main),
       }
     );
-    debug();
     await expect(document.body).toHaveNoAxeViolations();
     await expect(document.body).toHaveNoDAPViolations('StepIndicator');
   });
@@ -29,5 +28,21 @@ describe('StepIndicator', () => {
   test('should add a custom class to the parent component', () => {
     const { container } = render(<StepIndicator className="custom-class" />);
     expect(container.firstElementChild).toHaveClass('custom-class');
+  });
+
+  test('should add a custom class to the child step component', () => {
+    const { getByText } = render(
+      <StepIndicator>
+        <Step label="test label" className="custom-step-class" />
+      </StepIndicator>
+    );
+    expect(getByText(/test label/i).closest('li')).toHaveClass(
+      'custom-step-class'
+    );
+  });
+
+  test('should pass through extra prop to parent component via spread attriute', () => {
+    const { queryByTestId } = render(<StepIndicator data-testid="test-id" />);
+    expect(queryByTestId('test-id')).toBeInTheDocument();
   });
 });
