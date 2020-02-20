ad90d8d23a6d2427aad358f5738ab4a7eb567c1e
Jen Downs <hello@jendowns.com>
2020-02-19 20:05:49 -0600

    fix(status-indicator): add a11y tests and update test structure

--- src/components/StatusIndicator/__tests__/StatusIndicator.spec.js
+++ src/components/StatusIndicator/__tests__/StatusIndicator.spec.js
@@ -3,29 +3,72 @@
  * @copyright IBM Security 2019
  */
 
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 
-import { StatusStep, StatusIndicator } from '../../..';
+import { StatusIndicator, StatusStep } from '../../..';
 
-import props from '../_mocks_';
-
-const { statusIndicator: statusIndicatorProps, statusSteps } = props;
+import { STATUS } from '../StatusStep/StatusStep';
 
 describe('StatusIndicator', () => {
-  let statusIndicator;
-
-  beforeEach(() => {
-    statusIndicator = shallow(
-      <StatusIndicator {...statusIndicatorProps}>
-        {statusSteps.map(({ key, ...props }) => (
-          <StatusStep key={key} {...props} />
-        ))}
-      </StatusIndicator>
+  test('should have no Axe or DAP violations', async () => {
+    const main = document.createElement('main');
+    render(
+      <StatusIndicator
+        title="test title"
+        retry={{
+          label: 'test retry label',
+          action: () => {},
+        }}
+      >
+        <StatusStep />
+        <StatusStep status={STATUS.current} />
+        <StatusStep status={STATUS.complete} />
+        <StatusStep status={STATUS.error} errorMsg="test error" />
+      </StatusIndicator>,
+      {
+        // DAP requires a landmark '<main>' in the DOM:
+        container: document.body.appendChild(main),
+      }
     );
+    await expect(document.body).toHaveNoAxeViolations();
+    await expect(document.body).toHaveNoDAPViolations(`StatusIndicator`);
   });
 
-  it('renders', () => {
-    expect(statusIndicator).toMatchSnapshot();
+  test('should add a custom class', () => {
+    const { container } = render(<StatusIndicator className="custom-class" />);
+    expect(container.firstElementChild).toHaveClass('custom-class');
+  });
+
+  test('should add a title', () => {
+    const { queryByText } = render(<StatusIndicator title="test title" />);
+    expect(queryByText(/test title/i)).toBeVisible();
+  });
+
+  test('should add a `retry` object with a `label`', () => {
+    const { queryByText } = render(
+      <StatusIndicator
+        retry={{
+          label: 'test retry label',
+          action: () => {},
+        }}
+      />
+    );
+    expect(queryByText(/test retry label/i)).toBeVisible();
+  });
+
+  test('should invoke onClick mock when `retry.action` is clicked', () => {
+    const onClickMock = jest.fn();
+    const { getByText } = render(
+      <StatusIndicator
+        retry={{
+          label: 'test retry label',
+          action: onClickMock,
+        }}
+      />
+    );
+    userEvent.click(getByText(/test retry label/i));
+    expect(onClickMock).toHaveBeenCalledTimes(1);
   });
 });
