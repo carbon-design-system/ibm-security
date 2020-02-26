932b64a8b94675cdde925fb09b9f1726736ddbbb
Jen Downs <hello@jendowns.com>
2020-02-26 11:29:42 -0600

    fix(trending-card): add additiona props tests

--- src/components/TrendingCard/__tests__/TrendingCard.spec.js
+++ src/components/TrendingCard/__tests__/TrendingCard.spec.js
@@ -28,7 +28,7 @@ describe('TrendingCard', () => {
     await expect(document.body).toHaveNoDAPViolations('TrendingCard');
   });
 
-  test('should accept a custom link', () => {
+  test('should accept a custom link with its own attributes via `element`', () => {
     const { getByText, queryByTestId } = render(
       <TrendingCard
         title="test title"
@@ -61,5 +61,31 @@ describe('TrendingCard', () => {
       'href',
       '#test-href'
     );
+  });
+
+  test('should apply a title via `title`', () => {
+    const { queryByText } = render(<TrendingCard title="test title" />);
+    expect(queryByText(/test title/i)).toBeVisible();
+  });
+
+  test('should apply a subtitle via `subtitle`', () => {
+    const { queryByText } = render(
+      <TrendingCard title="test title" subtitle="test subtitle" />
+    );
+    expect(queryByText(/test subtitle/i)).toBeVisible();
+  });
+
+  test('should apply a custom class name', () => {
+    const { queryByText } = render(
+      <TrendingCard title="test title" className="custom-class" />
+    );
+    expect(queryByText(/test title/i).closest('a')).toHaveClass('custom-class');
+  });
+
+  test('should add extra props via spread attribute', () => {
+    const { queryByTestId } = render(
+      <TrendingCard title="test title" data-testid="test-id" />
+    );
+    expect(queryByTestId('test-id')).toBeVisible();
   });
 });
