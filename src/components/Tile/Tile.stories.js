1d1c37633384c2c830c8692e6402ee6135934dff
Simon Finney <SIMONFIN@ie.ibm.com>
2020-03-10 17:29:27 +0000

    refactor(tile): revert story, refactor styles

--- src/components/Tile/Tile.stories.js
+++ src/components/Tile/Tile.stories.js
@@ -4,7 +4,7 @@
  */
 
 import { action } from '@storybook/addon-actions';
-import { boolean, number, text } from '@storybook/addon-knobs';
+import { boolean, number, select, text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 
 import React from 'react';
@@ -12,13 +12,22 @@ import React from 'react';
 import { components } from '../../../.storybook';
 
 import {
-  Tile,
   ClickableTile,
   ExpandableTile,
+  RadioTile,
   SelectableTile,
+  Tile,
   TileAboveTheFoldContent,
   TileBelowTheFoldContent,
+  TileGroup,
 } from '../..';
+
+const radioValues = {
+  None: '',
+  standard: 'standard',
+  'default-selected': 'default-selected',
+  selected: 'selected',
+};
 
 const props = {
   selectable: () => ({
@@ -26,6 +35,23 @@ const props = {
     handleClick: action('handleClick'),
     handleKeyDown: action('handleKeyDown'),
   }),
+
+  group: () => ({
+    name: text('Form item (name in <TileGroup>)', 'tile-group'),
+    valueSelected: select(
+      'Value of the selected item (valueSelected in <TileGroup>)',
+      radioValues,
+      ''
+    ),
+    onChange: action('onChange'),
+  }),
+
+  radio: () => ({
+    name: text('Form item name (name in <RadioTile>)', 'tiles'),
+    onChange: action('onChange'),
+    light: boolean('Light variant (light)', false),
+  }),
+
   expandable: () => ({
     tabIndex: number('Tab index (tabIndex)', 0),
     expanded: boolean('Expanded (expanded)', false),
@@ -88,6 +114,42 @@ storiesOf(components('Tile'), module)
         text: `
             Selectable tile
             Use this to select multiple tiles.
+          `,
+      },
+    }
+  )
+  .add(
+    'Radio',
+    () => {
+      const radioProps = props.radio();
+      return (
+        <TileGroup
+          defaultSelected="default-selected"
+          legend="Radio Tile Group"
+          {...props.group()}
+        >
+          <RadioTile value="standard" {...radioProps}>
+            Radio Tile
+          </RadioTile>
+          <RadioTile value="default-selected" id="tile-2" {...radioProps}>
+            Radio Tile
+          </RadioTile>
+          <RadioTile value="selected" id="tile-3" {...radioProps}>
+            Radio Tile
+          </RadioTile>
+        </TileGroup>
+      );
+    },
+    {
+      info: {
+        text: `
+             The example below shows a Tile Group component with a default selected Tile.
+             Although you can set the checked prop on the Tile, when using the RadioTile component
+             as a child of the Tile Group, either set the defaultSelected or valueSelected which will
+             automatically set the selected prop on the corresponding RadioTile component.
+             Use defaultSelected when you want a tile to be selected initially, but don't need to set it
+             at a later time. If you do need to set it dynamically at a later time, then use the valueSelected property instead.
+             Use this to select one tile at a time.
           `,
       },
     }
