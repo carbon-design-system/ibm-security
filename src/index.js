746679789a0ec84936407561ba1a933a0886d6fc
Simon Finney <SIMONFIN@ie.ibm.com>
2020-03-11 18:03:14 +0000

    fix(list-box): export component, reorganise exports

--- src/index.js
+++ src/index.js
@@ -6,9 +6,22 @@
 // Globals.
 export theme from './globals/theme';
 
-// Application.
-export ErrorPage from './components/ErrorPage';
+// Components.
+export {
+  Accordion,
+  AccordionItem,
+  AccordionSkeleton,
+} from './components/Accordion';
+
+export {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbSkeleton,
+} from './components/Breadcrumb';
+
+export Button, { ButtonSkeleton } from './components/Button';
 export { Card, CardSkeleton } from './components/Card';
+export Checkbox, { CheckboxSkeleton } from './components/Checkbox';
 export CodeSnippet, { CodeSnippetSkeleton } from './components/CodeSnippet';
 export ComboBox from './components/ComboBox';
 export ComboButton, { ComboButtonItem } from './components/ComboButton';
@@ -38,13 +51,28 @@ export {
   TableToolbar,
   TableToolbarAction,
   TableToolbarContent,
+  TableToolbarDownload,
   TableToolbarSearch,
   TableToolbarMenu,
-  TableToolbarDownload,
 } from './components/DataTable';
 
+export {
+  DatePicker,
+  DatePickerInput,
+  DatePickerSkeleton,
+} from './components/DatePicker';
+
 export DelimitedList from './components/DelimitedList';
+export { Dropdown, DropdownSkeleton } from './components/Dropdown';
+export ErrorPage from './components/ErrorPage';
 export ExternalLink from './components/ExternalLink';
+
+export FileUploader, {
+  Filename,
+  FileUploaderButton,
+  FileUploaderSkeleton,
+} from './components/FileUploader';
+
 export FilterPanel, {
   FilterPanelAccordion,
   FilterPanelAccordionItem,
@@ -53,19 +81,33 @@ export FilterPanel, {
   FilterPanelLabel,
   FilterPanelSearch,
 } from './components/FilterPanel';
+
+export Form from './components/Form';
+export FormGroup from './components/FormGroup';
+export Header from './components/Header';
 export { ICA, ICASkeleton } from './components/ICA';
 export Icon from './components/Icon';
-export IconButtonBar from './components/IconButtonBar';
 export IconButton from './components/IconButton';
+export IconButtonBar from './components/IconButtonBar';
 export InlineLoading from './components/InlineLoading';
+export Link from './components/Link';
+export Loading, { LoadingMessage } from './components/Loading';
+
 export MultiSelect from './components/MultiSelect';
+export Modal from './components/Modal';
+export ModalWrapper from './components/ModalWrapper';
+export Nav, { NavItem, NavList } from './components/Nav';
 export NonEntitledSection from './components/NonEntitledSection';
+
 export {
   InlineNotification,
   NotificationActionButton,
   ToastNotification,
 } from './components/Notification';
+
 export NumberInput, { NumberInputSkeleton } from './components/NumberInput';
+export OverflowMenu from './components/OverflowMenu';
+export OverflowMenuItem from './components/OverflowMenuItem';
 export Pagination from './components/Pagination';
 
 export {
@@ -76,28 +118,55 @@ export {
 } from './components/Panel';
 
 export PanelV2 from './components/PanelV2';
-
 export Pill from './components/Pill';
 export Portal from './components/Portal';
+export ProfileImage from './components/ProfileImage';
 
 export {
   ProgressIndicator,
-  ProgressStep,
   ProgressIndicatorSkeleton,
+  ProgressStep,
 } from './components/ProgressIndicator';
 
+export RadioButton, { RadioButtonSkeleton } from './components/RadioButton';
+export RadioButtonGroup from './components/RadioButtonGroup';
+export RadioTile from './components/RadioTile';
+export ScrollGradient from './components/ScrollGradient';
+
 export Search, {
-  SearchSkeleton,
   SearchFilterButton,
   SearchLayoutButton,
+  SearchSkeleton,
 } from './components/Search';
 
 export SearchBar from './components/SearchBar';
+
+export {
+  Select,
+  SelectItem,
+  SelectItemGroup,
+  SelectSkeleton,
+} from './components/Select';
+
+export Shell from './components/Shell';
+export SkeletonText from './components/SkeletonText';
+export Slider, { SliderSkeleton } from './components/Slider';
 export StackedNotification from './components/StackedNotification';
 export StatusIcon from './components/StatusIcon';
-export { StatusStep, StatusIndicator } from './components/StatusIndicator';
+export { StatusIndicator, StatusStep } from './components/StatusIndicator';
 export { Step, StepIndicator } from './components/StepIndicator';
 export StringFormatter from './components/StringFormatter';
+
+export {
+  StructuredListBody,
+  StructuredListCell,
+  StructuredListHead,
+  StructuredListInput,
+  StructuredListRow,
+  StructuredListSkeleton,
+  StructuredListWrapper,
+} from './components/StructuredList';
+
 export Switch from './components/Switch';
 
 export SummaryCard, {
@@ -112,12 +181,33 @@ export SummaryCard, {
   SummaryCardSkeleton,
 } from './components/SummaryCard';
 
+export { Tab, Tabs, TabsSkeleton } from './components/Tabs';
 export Tag, { InteractiveTag, TagSkeleton } from './components/Tag';
 export TagWall from './components/TagWall';
 export TagWallFilter from './components/TagWallFilter';
 export { Tearsheet, TearsheetSmall } from './components/Tearsheet';
+export TextArea, { TextAreaSkeleton } from './components/TextArea';
 export TextInput, { TextInputSkeleton } from './components/TextInput';
+
+export {
+  ClickableTile,
+  ExpandableTile,
+  SelectableTile,
+  Tile,
+  TileAboveTheFoldContent,
+  TileBelowTheFoldContent,
+} from './components/Tile';
+
+export TileGroup from './components/TileGroup';
 export TimeIndicator from './components/TimeIndicator';
+export TimePicker from './components/TimePicker';
+export TimePickerSelect from './components/TimePickerSelect';
+export Toggle, { ToggleSkeleton } from './components/Toggle';
+export ToggleSmall, { ToggleSmallSkeleton } from './components/ToggleSmall';
+export Toolbar from './components/Toolbar';
+export Tooltip from './components/Tooltip';
+export TooltipDefintion from './components/TooltipDefinition';
+export Transition from './components/Transition';
 export TrendingCard from './components/TrendingCard';
 
 export {
@@ -126,91 +216,19 @@ export {
   TypeLayoutCell,
   TypeLayoutRow,
 } from './components/TypeLayout';
+
+export * from './components/UIShell';
 export { Wizard, WizardStep } from './components/Wizard';
 
-// Platform.
-export Header from './components/Header';
-export Nav, { NavItem, NavList } from './components/Nav';
-export ProfileImage from './components/ProfileImage';
-export Shell from './components/Shell';
-export Toolbar from './components/Toolbar';
+// Experimental.
+export UNSTABLE__Pagination, {
+  PageSelector,
+} from './components/UNSTABLE__Pagination';
 
-// Carbon proxy.
-export {
-  Accordion,
-  AccordionItem,
-  AccordionSkeleton,
-} from './components/Accordion';
-export {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbSkeleton,
-} from './components/Breadcrumb';
-export Button, { ButtonSkeleton } from './components/Button';
-export Checkbox, { CheckboxSkeleton } from './components/Checkbox';
-export {
-  DatePicker,
-  DatePickerInput,
-  DatePickerSkeleton,
-} from './components/DatePicker';
-export { Dropdown, DropdownSkeleton } from './components/Dropdown';
-export FileUploader, {
-  Filename,
-  FileUploaderButton,
-  FileUploaderSkeleton,
-} from './components/FileUploader';
-export Form from './components/Form';
-export FormGroup from './components/FormGroup';
-export Link from './components/Link';
-export Loading, { LoadingMessage } from './components/Loading';
-export Modal from './components/Modal';
-export ModalWrapper from './components/ModalWrapper';
-export OverflowMenu from './components/OverflowMenu';
-export OverflowMenuItem from './components/OverflowMenuItem';
-export RadioButton, { RadioButtonSkeleton } from './components/RadioButton';
-export RadioButtonGroup from './components/RadioButtonGroup';
-export RadioTile from './components/RadioTile';
-export ScrollGradient from './components/ScrollGradient';
-export {
-  Select,
-  SelectItem,
-  SelectItemGroup,
-  SelectSkeleton,
-} from './components/Select';
-export SkeletonText from './components/SkeletonText';
-export Slider, { SliderSkeleton } from './components/Slider';
-export {
-  StructuredListBody,
-  StructuredListCell,
-  StructuredListHead,
-  StructuredListInput,
-  StructuredListRow,
-  StructuredListWrapper,
-  StructuredListSkeleton,
-} from './components/StructuredList';
-export { Tabs, Tab, TabsSkeleton } from './components/Tabs';
-export {
-  Tile,
-  ClickableTile,
-  SelectableTile,
-  ExpandableTile,
-  TileAboveTheFoldContent,
-  TileBelowTheFoldContent,
-} from './components/Tile';
-export TileGroup from './components/TileGroup';
-export TimePicker from './components/TimePicker';
-export TimePickerSelect from './components/TimePickerSelect';
-export Toggle, { ToggleSkeleton } from './components/Toggle';
-export TextArea, { TextAreaSkeleton } from './components/TextArea';
-export ToggleSmall, { ToggleSmallSkeleton } from './components/ToggleSmall';
-export Tooltip from './components/Tooltip';
-export TooltipDefintion from './components/TooltipDefinition';
-export Transition from './components/Transition';
-export * from './components/UIShell';
-
-// Hidden Carbon proxy.
+// Carbon.
 export FormItem from 'carbon-components-react/lib/components/FormItem';
 export FormLabel from 'carbon-components-react/lib/components/FormLabel';
+export ListBox from 'carbon-components-react/lib/components/ListBox';
 export ListItem from 'carbon-components-react/lib/components/ListItem';
 
 export {
@@ -219,11 +237,5 @@ export {
 } from 'carbon-components-react/lib/components/Notification';
 
 export OrderedList from 'carbon-components-react/lib/components/OrderedList';
-
 export TabContent from 'carbon-components-react/lib/components/TabContent';
 export UnorderedList from 'carbon-components-react/lib/components/UnorderedList';
-
-// Unstable experimental components
-export UNSTABLE__Pagination, {
-  PageSelector,
-} from './components/UNSTABLE__Pagination';
