ee5d6dc92f248d3ddaba00b5a60c49d6384f8589
Jen Downs <hello@jendowns.com>
2020-05-13 08:22:17 -0500

    fix(datatable): rely on spread property for carbon props

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -1,10 +1,9 @@
 /**
  * @file Data table.
- * @copyright IBM Security 2019
+ * @copyright IBM Security 2019-2020
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import CarbonDataTable from 'carbon-components-react/lib/components/DataTable';
 
@@ -36,94 +35,20 @@ const {
   TableToolbarMenu,
 } = CarbonDataTable;
 
-const DataTable = ({
-  filterRows,
-  headers,
-
-  isSortable,
-  isSelectable,
-  render,
-  rows,
-  sortRow,
-
-  useZebraStyles,
-  ...other
-}) => (
-  <div className={namespace}>
-    <CarbonDataTable
-      filterRows={filterRows}
-      headers={headers}
-      isSortable={isSortable}
-      render={render}
-      rows={rows}
-      sortRow={sortRow}
-      useZebraStyles={useZebraStyles}
-      {...other}
-    />
-  </div>
-);
+function DataTable({ ...other }) {
+  return (
+    <div className={namespace}>
+      <CarbonDataTable {...other} />
+    </div>
+  );
+}
 
 DataTable.defaultProps = {
-  isSortable: true,
-  isSelectable: true,
-  render: undefined,
-  sortRow: undefined,
-  filterRows: undefined,
-  useZebraStyles: false,
+  ...CarbonDataTable.defaultProps,
 };
 
 DataTable.propTypes = {
-  /**
-   * @type {Object.<Object, *>}
-   * The `rows` prop is where you provide us with a list of all the rows that
-   * you want to render in the table. The only hard requirement is that this
-   * is an array of objects, and that each object has a unique `id` field
-   * available on it.
-   */
-  rows: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-
-  /**
-   * @type {Object.<Object, *>}
-   * The `headers` prop represents the order in which the headers should
-   * appear in the table. We expect an array of objects to be passed in, where
-   * `key` is the name of the key in a row object, and `header` is the name of
-   * the header.
-   */
-  headers: PropTypes.arrayOf(
-    PropTypes.shape({
-      key: PropTypes.string.isRequired,
-      header: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-
-  /** @type {function(rows: Array, headers: Array, ...props)}
-   * render function for the contents of the table.
-   */
-  render: PropTypes.func,
-
-  /** @type {bool} Defines whether the table is sortable. */
-  isSortable: PropTypes.bool,
-
-  /**
-   * @type {func} Optional hook to manually control sorting of the rows.
-   */
-  sortRow: PropTypes.func,
-
-  /**
-   * @type {func} Optional hook to manually control filtering of the rows from the
-   * TableToolbarSearch component
-   */
-  filterRows: PropTypes.func,
-
-  /** @type {bool} Defines whether the table is selectable. */
-  isSelectable: PropTypes.bool,
-
-  /** @type {bool} Defines whether the table uses zebra styles. */
-  useZebraStyles: PropTypes.bool,
+  ...CarbonDataTable.propTypes,
 };
 
 export const { defaultProps, propTypes } = DataTable;
