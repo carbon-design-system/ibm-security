4f67dd8d1b10a97a07f897ec760beca21f43b8d4
Simon Finney <SIMONFIN@ie.ibm.com>
2020-06-22 13:52:20 +0100

    docs(storybook): update documentation

--- src/components/UNSTABLE__Layout/stories/index.js
+++ src/components/UNSTABLE__Layout/stories/index.js
@@ -7,4 +7,4 @@ import { namespace } from '../LayoutModules/LayoutModule';
 
 export default `In combination with ['Grid' components](https://react.carbondesignsystem.com/?path=/story/grid--auto-columns), leverage 'LayoutModules' components as a foundation for horizontal layout. Spacing classes provide additional customisation to margin and padding, for example \`${namespace}--layout-01--pt\`
 
-- [Learn more](https://www.carbondesignsystem.com/guidelines/spacing#spacing-scale) about Carbon and additional values for spacing classes`;
+- [Learn more](https://www.carbondesignsystem.com/guidelines/spacing/overview#spacing-scale) about Carbon and additional values for spacing classes`;
