e14b94e55a568a22b2ce0b99b4d664fef2375dee
Simon Finney <SIMONFIN@ie.ibm.com>
2020-07-12 16:35:57 +0100

    docs(data-table): add `size` knob

--- src/components/DataTable/DataTable.stories.js
+++ src/components/DataTable/DataTable.stories.js
@@ -3,14 +3,17 @@
  * @copyright IBM Security 2019 - 2020
  */
 
-import { boolean } from '@storybook/addon-knobs';
+import { boolean, select } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 
 import { components } from '../../../.storybook';
 
+const sizes = [null, 'compact', 'short', 'tall'];
+
 const props = () => ({
-  useZebraStyles: boolean('Zebra row styles (useZebraStyles)', false),
+  size: select('Row height (size)', sizes, sizes[0]),
   stickyHeader: boolean('Sticky header (stickyHeader)', false),
+  useZebraStyles: boolean('Zebra row styles (useZebraStyles)', false),
 });
 
 const readmeURL = 'https://bit.ly/2Z9PGsC';
