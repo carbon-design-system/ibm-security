7c8026c6be77a45acbdc35487aa1865ad24cb6ae
Simon Finney <SIMONFIN@ie.ibm.com>
2020-07-22 15:47:49 +0100

    docs(icon): wrap story in `Fragment`

--- src/components/Icon/Icon.stories.js
+++ src/components/Icon/Icon.stories.js
@@ -1,40 +1,36 @@
 /**
  * @file Icon stories.
- * @copyright IBM Security 2019
+ * @copyright IBM Security 2019 - 2020
  */
 
-import Add20 from '@carbon/icons-react/lib/add/20';
-import Add24 from '@carbon/icons-react/lib/add/24';
-import Add32 from '@carbon/icons-react/lib/add/32';
-
+import { Add20, Add24, Add32 } from '@carbon/icons-react';
 import { miniUnits } from '@carbon/layout';
 
 import { storiesOf } from '@storybook/react';
-
 import React from 'react';
 
 import { components } from '../../../.storybook';
-
 import theme from '../../globals/theme';
-
-import { className } from '../_mocks_';
-import { renderIcon } from '../Icon/_mocks_';
 
 import { Icon } from '../..';
 
+import { renderIcon } from './_mocks_';
+
 const iconProps = {
-  className,
   style: { fill: theme.icon01, margin: miniUnits(1) },
 };
 
 storiesOf(components('Icon'), module).add(
-  'default',
-  () =>
-    [renderIcon, Add20, Add24, Add32].map((size, index) => {
-      const key = `icon__${index}`;
+  'Default',
+  () => (
+    <>
+      {[renderIcon, Add20, Add24, Add32].map((size, index) => {
+        const key = `icon__${index}`;
 
-      return <Icon key={key} renderIcon={size} {...iconProps} />;
-    }),
+        return <Icon key={key} renderIcon={size} {...iconProps} />;
+      })}
+    </>
+  ),
 
   {
     info: {
