bb408b3a160f8db3a086b285e8b688e61b205393
Simon Finney <simonjfinney@gmail.com>
2020-07-31 11:01:52 +0100

    fix(scroll-gradient): use `ResizeObserver` (#667)
    * fix(scroll-gradient): use `ResizeObserver`
    
    * fix(scroll-gradient): use `ResizeObserver`
    
    * fix(scroll-gradient): use `ResizeObserver`
    
    * fix(scroll-gradient): use `ResizeObserver`
    
    * chore(resize-observer): add polyfill
    
    * chore(resize-observer): add polyfill
    
    * test(scroll-gradient): update test structure
    
    * test(scroll-gradient): use separate mock
    
    * test(scroll-gradient): use separate mock
    
    * ci(scroll-gradient): fix SSR
    
    * test(scroll-gradient): scope `ResizeObserver` tests
    
    * chore(scroll-gradient): conditionally require polyfill

--- src/components/ScrollGradient/ScrollGradient.js
+++ src/components/ScrollGradient/ScrollGradient.js
@@ -1,19 +1,24 @@
 /**
  * @file Scroll gradient.
- * @copyright IBM Security 2019
+ * @copyright IBM Security 2019 - 2020
  */
 
-import React, { Component } from 'react';
 import classnames from 'classnames';
-import { throttle } from 'throttle-debounce';
 import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-import { isClient } from '../../globals/utils/capabilities';
+import { throttle } from 'throttle-debounce';
+
 import { getComponentNamespace } from '../../globals/namespace';
+import { isClient } from '../../globals/utils/capabilities';
 
 export const namespace = getComponentNamespace('scroll-gradient');
 
 const scrollDirection = { X: 'X', Y: 'Y' };
+
+// Checks if the native version is available and switch between this and the polyfill to improve performance on browsers with native support - https://www.npmjs.com/package/resize-observer-polyfill
+const ResizeObserver =
+  (isClient() && window.ResizeObserver) || require('resize-observer-polyfill');
 
 class ScrollGradient extends Component {
   static propTypes = {
@@ -96,7 +101,10 @@ class ScrollGradient extends Component {
     }
 
     if (isClient()) {
-      window.addEventListener('resize', this.updateHandler);
+      // https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver
+      this.resizeObserver = new ResizeObserver(this.updateHandler);
+
+      this.resizeObserver.observe(this.scrollContainer);
     }
   }
 
@@ -110,7 +118,7 @@ class ScrollGradient extends Component {
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this.updateHandler);
+    this.resizeObserver.disconnect();
   }
 
   /**
