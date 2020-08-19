7372ef680b5229d6362cd6c6d61d4a5bfbb6a2e0
Jen Downs <hello@jendowns.com>
2020-08-19 18:09:10 -0500

    feat(search-bar): apply titleText prop for inner multiselect (#723)
    * feat(search-bar): apply titleText prop for inner multiselect
    
    * docs(search-bar): add hideScopesLabel prop to story knobs
    
    Co-authored-by: Deriohig <deriohig@gmail.com>

--- src/components/SearchBar/SearchBar.stories.js
+++ src/components/SearchBar/SearchBar.stories.js
@@ -4,7 +4,7 @@
  */
 
 import { action } from '@storybook/addon-actions';
-import { text } from '@storybook/addon-knobs';
+import { boolean, text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 
 import React from 'react';
@@ -72,6 +72,7 @@ const storyProps = {
       allSelectedScopesLabel
     ),
     scopeToString: ({ name }) => name,
+    hideScopesLabel: boolean('Hide scopes label (hideScopesLabel)', true),
   }),
   translationIds: {
     'close.menu': 'Close the menu',
