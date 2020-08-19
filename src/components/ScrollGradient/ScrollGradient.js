279a6bc6601f09dd1768463f707c288ba858935b
Simon Finney <SIMONFIN@ie.ibm.com>
2020-08-19 09:59:50 +0100

    fix(resize-observer): allow library handle ponyfill

--- src/components/ScrollGradient/ScrollGradient.js
+++ src/components/ScrollGradient/ScrollGradient.js
@@ -7,6 +7,8 @@ import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
+// https://www.npmjs.com/package/resize-observer-polyfill
+import ResizeObserver from 'resize-observer-polyfill';
 import { throttle } from 'throttle-debounce';
 
 import { getComponentNamespace } from '../../globals/namespace';
@@ -15,10 +17,6 @@ import { isClient } from '../../globals/utils/capabilities';
 export const namespace = getComponentNamespace('scroll-gradient');
 
 const scrollDirection = { X: 'X', Y: 'Y' };
-
-// Checks if the native version is available and switch between this and the polyfill to improve performance on browsers with native support - https://www.npmjs.com/package/resize-observer-polyfill
-const ResizeObserver =
-  (isClient() && window.ResizeObserver) || require('resize-observer-polyfill');
 
 class ScrollGradient extends Component {
   static propTypes = {
