30877dba9a2ae1d429b89258531930524a41bd0b
Simon Finney <SIMONFIN@ie.ibm.com>
2020-08-26 19:15:01 +0100

    feat(storybook): add `addon-docs` support

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -8,6 +8,7 @@ import '@storybook/addon-storysource/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-a11y/register';
+import '@storybook/addon-docs/register';
 
 import '@storybook/addon-viewport/register';
 
