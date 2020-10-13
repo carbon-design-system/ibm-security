ecfb8eb5f873751d460e2b213364e19dee98ead2
Simon Finney <simonjfinney@gmail.com>
2020-10-13 16:46:48 +0100

    docs(storybook): add and update configuration to support custom MDX (#788)
    * docs(storybook): add configuration to support custom MDX
    
    * docs(storybook): fix theme order
    
    * build(addon-links): add `addon-links` package

---
+++ .storybook/components/Canvas/index.js
@@ -0,0 +1,9 @@
+/**
+ * @file Canvas.
+ * @copyright IBM Security 2020
+ */
+
+import { Canvas } from '@storybook/addon-docs/blocks';
+import React from 'react';
+
+export default props => <Canvas withSource="open" {...props} />;
