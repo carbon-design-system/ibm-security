d4f302a5a4f1fc8bd9f06259925960bf4daa8f66
Simon Finney <SIMONFIN@ie.ibm.com>
2020-10-14 20:06:09 +0100

    docs(storybook): disable scrolling in `Docs` tab

--- .storybook/components/DocsContainer/index.js
+++ .storybook/components/DocsContainer/index.js
@@ -7,8 +7,8 @@ import { Anchor, DocsContainer } from '@storybook/addon-docs/blocks';
 import React from 'react';
 
 // https://github.com/storybookjs/storybook/issues/10983
-export default ({ context, children }) => (
-  <DocsContainer context={context}>
+export default ({ children, ...props }) => (
+  <DocsContainer {...props}>
     <Anchor storyId={new URLSearchParams(window.location.search).get('id')} />
     {children}
   </DocsContainer>
