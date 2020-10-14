5386a004b2d0741e832bbbda844e4478250be0d7
Simon Finney <SIMONFIN@ie.ibm.com>
2020-10-14 13:07:42 +0100

    feat(layout-modules): add `ICAModule`

---
+++ src/components/UNSTABLE__LayoutModules/ICAModule/ICAModule.stories.js
@@ -0,0 +1,60 @@
+/**
+ * @file ICA module stories.
+ * @copyright IBM Security 2020
+ */
+
+import React from 'react';
+
+import { getDocsParameters } from '../../../../.storybook';
+
+import { Column, ICA, ICAModule, Row } from '../../..';
+
+import getTitle from '../stories';
+
+export default {
+  title: getTitle(ICAModule),
+  component: ICAModule,
+  parameters: {
+    ...getDocsParameters(),
+  },
+};
+
+export const Default = () => (
+  <ICAModule>
+    {() => (
+      <Row>
+        <Column>
+          <ICA label="Label" value={100} />
+        </Column>
+
+        <Column>
+          <ICA label="Label" value={100} />
+        </Column>
+
+        <Column>
+          <ICA label="Label" value={100} />
+        </Column>
+      </Row>
+    )}
+  </ICAModule>
+);
+
+export const Hover = () => (
+  <ICAModule hover>
+    {({ getLayoutProps }) => (
+      <Row>
+        <Column {...getLayoutProps()}>
+          <ICA label="Label" value={100} />
+        </Column>
+
+        <Column {...getLayoutProps()}>
+          <ICA label="Label" value={100} />
+        </Column>
+
+        <Column {...getLayoutProps()}>
+          <ICA label="Label" value={100} />
+        </Column>
+      </Row>
+    )}
+  </ICAModule>
+);
