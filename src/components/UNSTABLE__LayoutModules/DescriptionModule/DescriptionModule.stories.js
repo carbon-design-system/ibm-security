8742f65677bb30afb76facc92eba0fc1f77325bb
Simon Finney <simonjfinney@gmail.com>
2020-10-14 12:19:52 +0100

    feat(layout-modules): add `DescriptionModule` (#792)
    * feat(layout-modules): add `DescriptionModule`
    
    * docs(description-module): update `propTypes` content
    
    * chore(description-module): update margin
    
    * chore(description-module): fix broken improt

---
+++ src/components/UNSTABLE__LayoutModules/DescriptionModule/DescriptionModule.stories.js
@@ -0,0 +1,83 @@
+/**
+ * @file Description module stories.
+ * @copyright IBM Security 2020
+ */
+
+import React from 'react';
+
+import { getDocsParameters } from '../../../../.storybook';
+import withResponsive from '../../../../.storybook/decorators';
+
+import { DescriptionModule, TitleBarModule } from '../../..';
+
+import getTitle from '../stories';
+
+export default {
+  title: getTitle(DescriptionModule),
+  component: DescriptionModule,
+  parameters: {
+    ...getDocsParameters(),
+  },
+  decorators: [withResponsive],
+};
+
+export const Default = () => (
+  <DescriptionModule>
+    {({ getLayoutProps }) => (
+      <>
+        <TitleBarModule title="Section title" />
+
+        <p {...getLayoutProps()}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          finibus tortor eget est ornare, a ultrices risus tincidunt.
+          Suspendisse condimentum mauris at ornare tempor. Phasellus a arcu
+          ante. Morbi vitae ultrices quam, eget eleifend magna. Morbi quis
+          porttitor ex, in elementum tellus. In eget rutrum eros, eu scelerisque
+          risus. Phasellus vel pretium lorem, ut laoreet sapien. Cras ac purus
+          vitae velit efficitur iaculis. Nunc.
+        </p>
+
+        <p {...getLayoutProps()}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          finibus tortor eget est ornare, a ultrices risus tincidunt.
+          Suspendisse condimentum mauris at ornare tempor. Phasellus a arcu
+          ante. Morbi vitae ultrices quam, eget eleifend magna. Morbi quis
+          porttitor ex, in elementum tellus. In eget rutrum eros, eu scelerisque
+          risus. Phasellus vel pretium lorem, ut laoreet sapien. Cras ac purus
+          vitae velit efficitur iaculis. Nunc.
+        </p>
+      </>
+    )}
+  </DescriptionModule>
+);
+
+export const SubsectionTitle = () => (
+  <DescriptionModule>
+    {({ getLayoutProps }) => (
+      <>
+        <TitleBarModule title="Section title" />
+        <TitleBarModule title="Subsection title" subsection />
+
+        <p {...getLayoutProps()}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          finibus tortor eget est ornare, a ultrices risus tincidunt.
+          Suspendisse condimentum mauris at ornare tempor. Phasellus a arcu
+          ante. Morbi vitae ultrices quam, eget eleifend magna. Morbi quis
+          porttitor ex, in elementum tellus. In eget rutrum eros, eu scelerisque
+          risus. Phasellus vel pretium lorem, ut laoreet sapien. Cras ac purus
+          vitae velit efficitur iaculis. Nunc.
+        </p>
+
+        <p {...getLayoutProps()}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          finibus tortor eget est ornare, a ultrices risus tincidunt.
+          Suspendisse condimentum mauris at ornare tempor. Phasellus a arcu
+          ante. Morbi vitae ultrices quam, eget eleifend magna. Morbi quis
+          porttitor ex, in elementum tellus. In eget rutrum eros, eu scelerisque
+          risus. Phasellus vel pretium lorem, ut laoreet sapien. Cras ac purus
+          vitae velit efficitur iaculis. Nunc.
+        </p>
+      </>
+    )}
+  </DescriptionModule>
+);
