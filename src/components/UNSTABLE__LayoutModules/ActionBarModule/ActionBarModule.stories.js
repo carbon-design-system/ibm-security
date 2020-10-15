0e269764767263d65d3df58a1141cdb8eb605713
Simon Finney <simonjfinney@gmail.com>
2020-10-15 13:20:45 +0100

    docs: rename supplementary stories to 'Variant', enforce 'Docs' tab if available (#794)
    * docs(layout-modules): update story naming
    
    * docs(storybook): enforce `Docs` tab by default if available
    
    * docs(storybook): hide `addon-controls` warning
    
    Co-authored-by: j1mie <jimmy.loughran1@ie.ibm.com>

--- src/components/UNSTABLE__LayoutModules/ActionBarModule/ActionBarModule.stories.js
+++ src/components/UNSTABLE__LayoutModules/ActionBarModule/ActionBarModule.stories.js
@@ -52,7 +52,7 @@ export const Default = () => (
   </ActionBarModule>
 );
 
-export const Details = () => (
+export const Variant = () => (
   <ActionBarModule>
     Supplementary details
     <ActionBarModuleItems>
