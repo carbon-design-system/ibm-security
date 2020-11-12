8dc6fdd71647e49a190e7749f71fac64c79241f0
Simon Finney <simonjfinney@gmail.com>
2020-11-12 18:04:27 +0000

    build(analytics): fix analytics property (#836)

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -10,3 +10,7 @@ import theme from './theme';
 addons.setConfig({
   theme,
 });
+
+// Set the Google Analytics tracking ID if the `master` branch is deployed.
+window.STORYBOOK_GA_ID =
+  process.env.STORYBOOK_BRANCH === 'master' && 'UA-80770450-3';
