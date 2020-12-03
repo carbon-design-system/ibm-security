111a526dc539d65d8a943c8153687c5e8a41d3ed
Simon Finney <SIMONFIN@ie.ibm.com>
2020-12-03 14:42:53 +0000

    feat(navigation-card): add logic, story, and styles

---
+++ src/components/NavigationCard/NavigationCard.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import NavigationCard from '.';
+
+export default {
+  title: 'NavigationCard',
+};
+
+export const Default = () => <NavigationCard label="Card" link="Link" />;
---
+++ src/components/NavigationCard/index.js
@@ -0,0 +1,27 @@
+import { ArrowRight16 } from '@carbon/icons-react';
+
+import { string } from 'prop-types';
+import React from 'react';
+
+import Card from '../Card';
+import Link from '../Link';
+
+const NavigationCard = ({ label, link }) => (
+  <Card>
+    <ArrowRight16 className="icon" />
+
+    <h3 className="label">{label}</h3>
+
+    <Link href="#">{link}</Link>
+  </Card>
+);
+
+NavigationCard.propTypes = {
+  /** Provide the label of the `NavigationCard`. */
+  label: string.isRequired,
+
+  /** Provide the contents of the `Link`. */
+  link: string.isRequired,
+};
+
+export default NavigationCard;
