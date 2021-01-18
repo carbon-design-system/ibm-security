72fbebfc2dd2b4d4173537b1564b2e90b309a422
Simon Finney <simonjfinney@gmail.com>
2021-01-18 11:08:04 +0000

    fix(filter-panel): define checkbox overflow container (#886)
    * feat(carbon): upgrade dependencies
    
    * test(filter-panel-search): update to test immediate functionality
    
    * fix(combo-button): remove scrolling workaround
    
    * fix(filter-panel): define checkbox overflow container
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- src/components/FilterPanel/FilterPanelCheckboxWithOverflowMenu/FilterPanelCheckboxWithOverflowMenu.js
+++ src/components/FilterPanel/FilterPanelCheckboxWithOverflowMenu/FilterPanelCheckboxWithOverflowMenu.js
@@ -9,8 +9,8 @@ import React, { useEffect, useRef, useState } from 'react';
 
 import { getComponentNamespace } from '../../../globals/namespace';
 
-import OverflowMenu, { getMenuOffset } from '../../OverflowMenu';
 import FilterPanelCheckbox from '../FilterPanelCheckbox';
+import OverflowMenu from '../../OverflowMenu';
 
 export const namespace = getComponentNamespace(
   'filter-panel-checkbox-with-overflow-menu'
@@ -34,28 +34,6 @@ const FilterPanelCheckboxWithOverflowMenu = ({
     !containerRef.current.contains(activeElement) &&
     setShowOverflowButton(false);
 
-  /**
-   * Sets the width of the overflow menu to match the width of this component's width and adjusts
-   * its position so it is right alingned with the overflow menu button.
-   *
-   * @param {HTMLElement} menuBody Element containing the overflow menu options.
-   * @param {string} direction The menu position relative to the menu button. Not used.
-   * @param {HTMLElement} menuButton Overflow menu button element.
-   * @returns {{top: number, left: number}} The menu offset measurements.
-   */
-  const updateMenuWidthAndSetOffset = (
-    menuBody,
-    direction,
-    menuButton,
-    ...args
-  ) => {
-    if (menuButton) {
-      menuBody.style.width = `${containerRef.current.clientWidth}px`;
-    }
-
-    return getMenuOffset(menuBody, direction, menuButton, ...args);
-  };
-
   useEffect(() => setShowOverflowButton(overflowIsOpen), [overflowIsOpen]);
 
   return (
@@ -68,6 +46,7 @@ const FilterPanelCheckboxWithOverflowMenu = ({
       onMouseOver={() => setShowOverflowButton(true)}
       onMouseLeave={() => hideOverflowButton(document.activeElement)}
       ref={containerRef}
+      data-floating-menu-container
     >
       <FilterPanelCheckbox
         wrapperClassName={`${namespace}__wrapper`}
@@ -79,7 +58,6 @@ const FilterPanelCheckboxWithOverflowMenu = ({
           open={open}
           className={`${namespace}__overflow-button`}
           menuOptionsClass={`${namespace}__overflow-options`}
-          menuOffsetFlip={updateMenuWidthAndSetOffset}
           ariaLabel={overflowMenuAriaLabel}
           iconDescription={overflowMenuAriaLabel}
           onOpen={() => setOverflowIsOpen(true)}
