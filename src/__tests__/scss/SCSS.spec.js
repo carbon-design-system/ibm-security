98a22c5996e7bcd7994569f8167bca30f49c634a
Simon Finney <SIMONFIN@ie.ibm.com>
2021-03-16 19:27:28 +0000

    refactor(sass-modules): update

--- src/__tests__/scss/SCSS.spec.js
+++ src/__tests__/scss/SCSS.spec.js
@@ -1,15 +1,14 @@
 /**
  * @file SCSS tests.
  * @copyright IBM Security 2020 - 2021
+ *
+ * @jest-environment node
  */
 
-import { resolve } from 'path';
 import { compile, forEachImport } from '../../../scripts/scss/compile';
 
 describe('SCSS', () => {
   test('Bundle', () => {
-    console.log(__dirname);
-
     expect(
       compile('src/index.scss')
         .css.toString()
@@ -20,8 +19,6 @@ describe('SCSS', () => {
   describe('Imports', () => {
     forEachImport(file => {
       test(file, () => {
-        console.log(file);
-
         expect(compile(file)).not.toHaveProperty('message');
       });
     });
