cdfdf50d386428e0653e996e7b282551909c11d9
Lily Peng <ms.lilypeng@gmail.com>
2021-04-05 13:00:34 -0400

    feat(list-item-module): 10 stories with hover/link states

--- src/components/UNSTABLE__LayoutModules/ListItemModule/ListItemModule.stories.js
+++ src/components/UNSTABLE__LayoutModules/ListItemModule/ListItemModule.stories.js
@@ -3,13 +3,13 @@
  * @copyright IBM Security 2021
  */
 
-import { Bee16, UserAvatar20 } from '@carbon/icons-react';
+import { Bee16, Locked16, UserAvatar20 } from '@carbon/icons-react';
 import React from 'react';
 
 import { getDocsParameters } from '../../../../.storybook';
 import withResponsive from '../../../../.storybook/decorators';
 
-import { ListItemModule } from '../../..';
+import { ICA, ListItemModule, ProfileImage, StatusIcon, Tag } from '../../..';
 
 import getTitle from '../stories';
 import page from './index.mdx';
@@ -26,7 +26,49 @@ export default {
 };
 
 export const Default = () => (
+  <ListItemModule href="#">
+    {({ Column, getLayoutProps }) => (
+      <>
+        <Column>
+          <h2 {...getLayoutProps({ type: 'title' })}>List title</h2>
+
+          <span {...getLayoutProps({ type: 'label' })}>Label</span>
+        </Column>
+      </>
+    )}
+  </ListItemModule>
+);
+
+export const Static = () => (
   <ListItemModule>
+    {({ Column, getLayoutProps }) => (
+      <>
+        <Column>
+          <h2 {...getLayoutProps({ type: 'title' })}>List title</h2>
+
+          <span {...getLayoutProps({ type: 'label' })}>Label</span>
+        </Column>
+      </>
+    )}
+  </ListItemModule>
+);
+
+export const AsButton = () => (
+  <ListItemModule onClick={() => console.log('hi')}>
+    {({ Column, getLayoutProps }) => (
+      <>
+        <Column>
+          <h2 {...getLayoutProps({ type: 'title' })}>List title</h2>
+
+          <span {...getLayoutProps({ type: 'label' })}>Label</span>
+        </Column>
+      </>
+    )}
+  </ListItemModule>
+);
+
+export const Detailed = () => (
+  <ListItemModule href="#">
     {({ Column, getLayoutProps }) => (
       <>
         <Column>
@@ -34,19 +76,172 @@ export const Default = () => (
         </Column>
 
         <Column>
-          <h2 {...getLayoutProps({ type: 'title' })}>Title</h2>
+          <h2 {...getLayoutProps({ type: 'title' })}>List title</h2>
 
-          <p {...getLayoutProps({ type: 'description' })}>Description</p>
-
-          <span {...getLayoutProps({ type: 'label' })}>Label</span>
+          <p {...getLayoutProps({ type: 'description' })}>
+            This is where the description goes. It can go up to three lines.
+          </p>
 
           <section {...getLayoutProps({ type: 'component' })}>
-            Component
+            Nested Component
           </section>
+
+          <span {...getLayoutProps({ type: 'label' })}>Time stamp / label</span>
 
           <UserAvatar20 {...getLayoutProps({ type: 'avatar' })} />
           <UserAvatar20 {...getLayoutProps({ type: 'avatar' })} />
           <UserAvatar20 {...getLayoutProps({ type: 'avatar' })} />
+        </Column>
+
+        <Column {...getLayoutProps({ type: 'right-column' })}>
+          <Locked16 />
+        </Column>
+      </>
+    )}
+  </ListItemModule>
+);
+
+export const WithProfileImage = () => (
+  <ListItemModule href="#">
+    {({ Column, getLayoutProps }) => (
+      <>
+        <Column>
+          <ProfileImage
+            {...getLayoutProps({ type: 'avatar' })}
+            profile={{
+              description: <span>Profile Image</span>,
+              image_url: null,
+              name: {
+                first_name: 'Sample',
+                surname: 'User',
+              },
+            }}
+          />
+        </Column>
+
+        <Column>
+          <p {...getLayoutProps({ type: 'description' })}>
+            This is where the description goes. It can go up to three lines.
+          </p>
+
+          <span {...getLayoutProps({ type: 'label' })}>Today 11:50 AM</span>
+        </Column>
+      </>
+    )}
+  </ListItemModule>
+);
+
+export const WithTag = () => (
+  <ListItemModule href="#">
+    {({ Column, getLayoutProps }) => (
+      <>
+        <Column {...getLayoutProps({ type: 'centered-column' })}>
+          <h2 {...getLayoutProps({ type: 'title' })}>Bill Callahan</h2>
+        </Column>
+
+        <Column {...getLayoutProps({ type: 'right-column' })}>
+          <Tag>16</Tag>
+        </Column>
+      </>
+    )}
+  </ListItemModule>
+);
+
+export const WithComponent = () => (
+  <ListItemModule href="#">
+    {({ Column, getLayoutProps }) => (
+      <>
+        <Column>
+          <Bee16 {...getLayoutProps({ type: 'top-icon' })} />
+
+          <h2 {...getLayoutProps({ type: 'title' })}>List title</h2>
+
+          <p {...getLayoutProps({ type: 'description' })}>
+            This is where the description goes. It can go up to three lines.
+          </p>
+
+          <section {...getLayoutProps({ type: 'component' })}>
+            <ICA label="Label" value={100} />
+            {/* <Button
+              iconDescription="Button icon"
+              kind="ghost"
+              largeText={null}
+              onClick={function noRefCheck(){}}
+              onFocus={function noRefCheck(){}}
+              renderIcon={Add16}
+              size="field"
+              tabIndex={0}
+              tooltipAlignment="center"
+              tooltipPosition="top"
+              type="button"
+            >
+              Add to canvas
+            </Button> */}
+          </section>
+        </Column>
+      </>
+    )}
+  </ListItemModule>
+);
+
+export const WithStatusIcon = () => (
+  <ListItemModule href="#">
+    {({ Column, getLayoutProps }) => (
+      <>
+        <Column>
+          <h2 {...getLayoutProps({ type: 'title' })}>
+            GPON vulnerability exploited
+          </h2>
+
+          <span {...getLayoutProps({ type: 'label' })}>Threat activity</span>
+        </Column>
+
+        <Column {...getLayoutProps({ type: 'right-column' })}>
+          <StatusIcon iconDescription="Status Icon" size="lg" status="info" />
+        </Column>
+      </>
+    )}
+  </ListItemModule>
+);
+
+export const WithInProgressStatus = () => (
+  <ListItemModule href="#">
+    {({ Column, getLayoutProps }) => (
+      <>
+        <Column>
+          <Bee16 {...getLayoutProps({ type: 'icon' })} />
+        </Column>
+
+        <Column>
+          <h2 {...getLayoutProps({ type: 'title' })}>Create new doc types</h2>
+
+          <UserAvatar20 {...getLayoutProps({ type: 'avatar' })} />
+          <UserAvatar20 {...getLayoutProps({ type: 'avatar' })} />
+          <UserAvatar20 {...getLayoutProps({ type: 'avatar' })} />
+        </Column>
+      </>
+    )}
+  </ListItemModule>
+);
+
+export const Columns = () => (
+  <ListItemModule href="#">
+    {({ Column, getLayoutProps }) => (
+      <>
+        <Column>
+          <Bee16 {...getLayoutProps({ type: 'icon' })} />
+        </Column>
+
+        <Column {...getLayoutProps({ type: 'narrow-column' })}>
+          <h2 {...getLayoutProps({ type: 'title' })}>Asset_name</h2>
+        </Column>
+
+        <Column {...getLayoutProps({ type: 'narrow-column' })}>
+          <h2 {...getLayoutProps({ type: 'title' })}>Data set</h2>
+        </Column>
+
+        <Column {...getLayoutProps({ type: 'narrow-column' })}>
+          <h2 {...getLayoutProps({ type: 'title' })}>3:47 pm</h2>
         </Column>
       </>
     )}
--- src/components/UNSTABLE__LayoutModules/ListItemModule/index.js
+++ src/components/UNSTABLE__LayoutModules/ListItemModule/index.js
@@ -4,7 +4,7 @@
  */
 
 import classnames from 'classnames';
-import { func } from 'prop-types';
+import { elementType, func, string } from 'prop-types';
 import React from 'react';
 
 import LayoutModule, { layoutModuleNamespace } from '../LayoutModule';
@@ -14,26 +14,80 @@ const namespace = 'list-item';
 /**
  * TODO: Description.
  */
-const ListItemModule = ({ children, ...other }) => (
-  <LayoutModule namespace={namespace} {...other}>
-    {children({
-      Column: props => (
-        <LayoutModule namespace={`${namespace}__column`} {...props} />
-      ),
+const ListItemModule = ({ children, as, href, onClick, ...other }) => {
+  const content = children({
+    Column: props => (
+      <LayoutModule namespace={`${namespace}__column`} {...props} />
+    ),
 
-      getLayoutProps: ({ className, type, ...rest } = {}) => ({
-        className: classnames(className, {
-          [`${layoutModuleNamespace}--${namespace}__${type}`]: type,
-        }),
-        ...rest,
+    getLayoutProps: ({ className, type, ...rest } = {}) => ({
+      className: classnames(className, {
+        [`${layoutModuleNamespace}--${namespace}__${type}`]: type,
       }),
-    })}
-  </LayoutModule>
-);
+      ...rest,
+    }),
+  });
+
+  let component = 'div';
+
+  if (as) {
+    component = as;
+  } else if (href) {
+    component = 'a';
+  } else if (onClick) {
+    component = 'button';
+  }
+
+  // div under button which is a flexbox with text-align: left
+
+  return (
+    <LayoutModule
+      namespace={`${namespace} ${layoutModuleNamespace}--${namespace}--${component}`}
+      as={component}
+      href={href}
+      onClick={onClick}
+      {...other}
+    >
+      {content}
+    </LayoutModule>
+  );
+};
+
+/*    <>
+      {hover ? (
+        <LayoutModule namespace={`${namespace} ${layoutModuleNamespace}--${namespace}--hover`} as={'a'} href="#" {...other}>
+          {content}
+        </LayoutModule>
+      ) : (
+        <LayoutModule namespace={namespace} {...other}>
+          {content}
+        </LayoutModule>
+      )}
+    </> */
 
 ListItemModule.propTypes = {
   /** Provide the content for the `ListItemModule` */
   children: func.isRequired,
+
+  /** Provide a custom element to be rendered instead of the default */
+  as: elementType,
+
+  /**
+   * Optionally specify an href for the ListItemModule to become an `<a>` element
+   */
+  href: string,
+
+  /**
+   * Provide an optional function to be called when the ListItemModule
+   * is clicked, turning the ListItemModule into a `<button>` element
+   */
+  onClick: func,
+};
+
+ListItemModule.defaultProps = {
+  as: 'div',
+  href: null,
+  onClick: null,
 };
 
 export default ListItemModule;
