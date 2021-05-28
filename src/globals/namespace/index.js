1fc84d6fb83ce8b2158eb80d4cbc77b0d43c4bb2
Dominik Brugger <dbrugger@de.ibm.com>
2021-05-28 16:06:55 +0200

    fix: change require to import syntax to allow treeshaking again

--- src/globals/namespace/index.js
+++ src/globals/namespace/index.js
@@ -3,9 +3,9 @@
  * @copyright IBM Security 2018 - 2021
  */
 
-const {
-  settings: { prefix: carbonPrefix },
-} = require('carbon-components');
+import { settings } from 'carbon-components';
+
+const carbonPrefix = settings.prefix;
 
 const namespace = 'security--';
 
