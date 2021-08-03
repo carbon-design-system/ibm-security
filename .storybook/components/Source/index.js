125b5352684abe4753d3acecea574e65c753a16f
Simon Finney <SIMONFIN@ie.ibm.com>
2021-08-03 18:41:40 +0100

    chore(storybook(: upgrade dependencies

--- .storybook/components/Source/index.js
+++ .storybook/components/Source/index.js
@@ -1,9 +1,9 @@
 /**
  * @file Source.
- * @copyright IBM Security 2020
+ * @copyright IBM Security 2020 - 2021
  */
 
-import { Source } from '@storybook/addon-docs/blocks';
+import { Source } from '@storybook/addon-docs';
 import React from 'react';
 
 export default (props) => <Source dark {...props} />;
