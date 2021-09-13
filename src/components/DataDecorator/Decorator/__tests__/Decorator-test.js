12bc9d0b1399c87185fd4888127e6ad57780eca6
Paul Balchin <52505287+paul-balchin-ibm@users.noreply.github.com>
2021-09-13 08:21:13 -0400

    Update src/components/DataDecorator/Decorator/__tests__/Decorator-test.js
    Co-authored-by: Simon Finney <simonjfinney@gmail.com>

--- src/components/DataDecorator/Decorator/__tests__/Decorator-test.js
+++ src/components/DataDecorator/Decorator/__tests__/Decorator-test.js
@@ -5,7 +5,6 @@
 
 import { fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { render } from '@testing-library/react';
 
 import React from 'react';
 
