94fd05de06b47b5fa19055f67e79ad7c57f93c91
Simon Finney <SIMONFIN@ie.ibm.com>
2021-10-22 21:07:08 +0100

    test: use `sass` for tests

--- src/__tests__/scss/styles-test.js
+++ src/__tests__/scss/styles-test.js
@@ -3,19 +3,32 @@
  * @copyright IBM Security 2020 - 2021
  */
 
-import { renderSync } from 'node-sass';
+import { sync } from 'glob';
 import { resolve } from 'path';
+import { exec } from 'shelljs';
 
 import { root } from '../../../config';
+
+function compile(input, options = []) {
+  return exec(
+    `sass -I ${resolve(root, 'node_modules')} -q ${options.join(' ')} ${input}`
+  );
+}
+
+const dirname = resolve(__dirname, '../..');
 
 describe('Styles', () => {
   test('Bundle', () => {
     expect(
-      renderSync({
-        file: resolve(__dirname, '../../index.scss'),
-        includePaths: [resolve(root, 'node_modules')],
-        outputStyle: 'expanded',
-      }).css.toString()
+      compile(resolve(dirname, 'index.scss')).toString()
     ).toMatchSnapshot();
+  });
+
+  test.concurrent.each(
+    sync(resolve(dirname, '**/*.scss'), {
+      ignore: '**/css-gridish/**',
+    })
+  )("Compile '%s'", async (input) => {
+    expect(compile(input, ['-s', 'compressed']).code).toBe(0);
   });
 });
